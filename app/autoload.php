45861222dc663dee0d7f61d876b108490d4c232e
Allan Cabral <allancruz93@gmail.com>
2021-03-12 00:42:17 -0300

    Adiciona os arquivos do banco de dados

---
+++ app/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+  spl_autoload_register(function($classe){
+
+    // Armazena os diretórios que serão verificados.
+    $a_diretorios = ["Libraries"];
+
+    // Percorre os diretórios informados.
+    foreach($a_diretorios as $diretorio)
+    {
+      // Armazena o caminho do arquivo.
+      $s_arquivo = __DIR__.DIRECTORY_SEPARATOR.$diretorio.DIRECTORY_SEPARATOR.$classe.".php";
+
+      // Verifica se o arquivo da classe dentro do diretório existe.
+      if(file_exists($s_arquivo))
+      {
+        require_once($s_arquivo);
+      }
+    }
+
+  });
+
+?>
